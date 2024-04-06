8b53cfd386d622a6505c3468b159036b9ba85896
crackstin <130881745+crackstin@users.noreply.github.com>
2024-04-05 20:45:12 -0400

    Update default.nix

--- pkgs/development/libraries/fltk/default.nix
+++ pkgs/development/libraries/fltk/default.nix
@@ -1,5 +1,5 @@
 import ./common.nix rec {
-  version = "1.3.8";
-  rev = "release-${version}";
-  sha256 = "1pw4ndwn9rr1cxw5qiw32r9la2z9zbjphgsqq1hj2yy4blwv419r";
+  version = "1.4.x-2021-12-21";
+  rev = "c8bb2a35850be7c6eaec5ad5a2936a77f7913de2";
+  sha256 = "1fwfg1hp1ajqh2b4ra4phi96854q9i8c0gbyi7pr35ljyv848295";
 }
